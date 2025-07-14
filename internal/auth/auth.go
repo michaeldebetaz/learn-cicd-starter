26a59735c525957662195c9d2fe5d4d578898b4b
michaeldebetaz <netarion@outlook.com>
2025-07-14 20:23:49 +0200

    feat: format my code to pass the ci

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -12,7 +12,8 @@ var (
 )
 
 // GetAPIKey -
-func GetAPIKey(headers http.Header)(string, error){ authHeader := headers.Get("Authorization")
+func GetAPIKey(headers http.Header) (string, error) {
+	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
